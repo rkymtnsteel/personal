631485c2bf0fff9064635b09a2d59e2453b34b60
Dean Geary <rkymtnsteel@gmail.com>
2025-02-10 15:14:52 -0500

    add hoh template

---
+++ hoh-template.py
@@ -0,0 +1,31 @@
+import ravendb
+
+server_url = ["http://192.168.1.131:8080"]
+database_name = "familybook"
+
+# Initialize the RavenDB session
+store = ravendb.DocumentStore(urls=server_url, database=database_name)
+store.initialize()
+
+# Define Head of household template
+# All ids will take the form: {family name} + generation + "-" seq 
+hoh_template = {
+    "@type": "headOfHousehold",
+    "familyName": "",
+    "id": "",
+    "fName": "",
+    "lName": "",
+    "generation": "",
+    "DOB": "",
+    "occupation": "",
+    "spouse": 
+        {
+            "id": "",
+        }
+}
+
+with store.open_session() as session:
+    session.store(hoh_template, "templates/hoh")
+    session.save_changes()
+    
+print("Template saved")
