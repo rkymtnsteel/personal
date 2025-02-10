86adee363cd1aa3b6ab9960025008c0977345ac0
Dean Geary <rkymtnsteel@gmail.com>
2025-02-10 16:32:52 -0500

    added @metadata

--- add-hoh-doc.py
+++ add-hoh-doc.py
@@ -7,12 +7,25 @@ database_name = "familybook"
 store = ravendb.DocumentStore(urls=server_url, database=database_name)
 store.initialize()
 
-def create_head_of_household(family_name, first_name, last_name, generation, dob, occupation, spouse_id):
+def generate_id(first_name, middle_name, last_name, generation):
+    first_initial = first_name[0].upper() if first_name else ""
+    middle_initial = middle_name[0].upper() if middle_name else "X"  # Use "X" if no middle name
+    last_part = last_name[:3].upper() if last_name else "XXX"  # First 3 letters of last name
+    return f"{first_initial}{middle_initial}{last_part}_{generation}"
+
+# Function to create a Head of Household document
+def create_head_of_household(first_name, middle_name, last_name, generation, dob, occupation):
+    hoh_id = generate_id(first_name, middle_name, last_name, generation)  # Generate custom ID
+    spouse_id = hoh_id + "_spouse"  # Spouse ID is HOH ID + "spouse"
+
+
     hoh_document = {
+        "@metadata": {"@collection": "hoh"},
         "@type": "headOfHousehold",
-        "familyName": family_name,
-        "id": f"{family_name.lower()}_{generation}",       # Unique ID based on family name & generation
+        "familyName": last_name,
+        "id": hoh_id,
         "fName": first_name,
+        "mName": middle_name,
         "lName": last_name,
         "generation": generation,
         "DOB": dob,
@@ -24,13 +37,12 @@ def create_head_of_household(family_name, first_name, last_name, generation, dob
 
 # Sample document using the template
 new_hoh = create_head_of_household(
-    family_name="Geary",
     first_name="Dean",
+    middle_name="A",
     last_name="Geary",
     generation="Gen1",
     dob="1955-10-31",
-    occupation="Software Engineer",
-    spouse_id="spouse_001"
+    occupation="Software Engineer"
 )
 
 # Save document to RavenDB
@@ -38,4 +50,4 @@ with store.open_session() as session:
     session.store(new_hoh, f"hoh/{new_hoh['id']}")  # Store under collection "hoh"
     session.save_changes()
 
-print("New Head of Household document added successfully!")
+print(f"New Head of Household document added with ID: {new_hoh['id']}")
